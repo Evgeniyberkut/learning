94f312a5f1608c516625f642d7d0ed18b4b94566
Evgeniy <johnnybrut@icloud.com>
2021-05-13 16:51:36 +0300

    third try

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.body.innerHTML = '<h1>Hello from Javascript</h1>'
 console. log('Test 2')
 document.body.innerText = '<h1>second test 2</h1>'
+document.body.innerHTML = '<h1>Hello from Javascript2</h1>'
